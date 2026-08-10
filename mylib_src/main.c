8cbea4494feb0e67ac51936c29fbfe35d9440abe
jiangmingpeng <431827848@qq.com>
2026-08-10 17:39:48 +0800

    动静态库的制作

---
+++ mylib_src/main.c
@@ -0,0 +1,41 @@
+#include "mystdio.h"
+#include <string.h>
+#include <unistd.h>
+#include "mystring.h"
+
+int main()
+{
+    myFILE *fp = myfopen("log.txt", "a");
+    if(fp == NULL)
+    {
+        printf("myfopen error!\n");
+        return 0;
+    }
+    
+    const char *msg = "hello jmp\n";
+    int cnt = 10;
+//    while(cnt--)
+//    {
+//        myfputs(msg, fp);
+//        sleep(1);
+//        printf("debug: outbuffer = %s, pos = %d\n", fp->Outbuffer, fp->pos);
+//    }
+    myfclose(fp);
+    printf("write file done!\n");
+    mystrlen();
+//    mvyFILE* fp = myfopen("log.txt","w");
+//    if(NULL==fp)
+//    {
+//        perror("myfopen");
+//        return -1;
+//    }
+//
+//    const char* str = "hello jmp";
+//    int cnt = 10;
+//    while(cnt--)
+//        myfputs(str,fp),sleep(1),cnt--;
+//
+//    myfclose(fp);
+
+    return 0;
+}
---
+++ mylib_src/mystring.c
@@ -0,0 +1,7 @@
+#include "mystring.h"
+
+#include <stdio.h>
+void mystrlen()
+{
+    printf("这是我的mystrlen!\n");
+}
